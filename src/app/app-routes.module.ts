33357d4d766df36b86a526be512f58fa0516bfbc
Vladimir Rodchenko <vladimir@aurochses.com>
2018-04-29 17:15:06 +0300

    !!! IMPORTANT !!! demo project ready

---
+++ src/app/app-routes.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: '**',
+        pathMatch: 'prefix',
+        redirectTo: ''
+    }
+];
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ]
+})
+export class AppRoutesModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { FormsModule } from '@aurochses/angular-forms';
 
+import { AppRoutesModule } from './app-routes.module';
 import { AppComponent } from './app.component';
+
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule,
+    AppRoutesModule,
     FormsModule
   ],
   providers: [],
