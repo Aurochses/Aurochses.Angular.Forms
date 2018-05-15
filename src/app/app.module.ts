44709dfed27ba4907434c2ef58f6b8901b4cffdf
Yauhen Sakovich <yauhen.sakovich@specific-group.com>
2018-05-15 11:24:38 +0300

    added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AurochsesFormModule } from 'angular-forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AurochsesFormModule
   ],
   providers: [],
   bootstrap: [AppComponent]
